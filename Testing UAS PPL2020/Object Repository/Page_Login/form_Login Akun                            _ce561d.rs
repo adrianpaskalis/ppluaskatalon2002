c16f954833c9a6f59ba1e339fd13cfde1c2e8959
adrianpaskal <adrian.paskalis@si.ukdw.ac.id>
2020-06-15 19:51:55 +0700

    initial commit

---
+++ Testing UAS PPL2020/Object Repository/Page_Login/form_Login Akun                            _ce561d.rs
@@ -0,0 +1,101 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>form_Login Akun                            _ce561d</name>
+   <tag></tag>
+   <elementGuidId>23ce0ae3-0b9b-41f1-a1ef-f4d5442104a9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//form[@action='https://testingjarkom.000webhostapp.com/services/auth/login.php']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>form</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>login100-form validate-form</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>action</name>
+      <type>Main</type>
+      <value>https://testingjarkom.000webhostapp.com/services/auth/login.php</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>method</name>
+      <type>Main</type>
+      <value>POST</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+        
+          Login Akun
+        
+
+        
+          
+          
+          
+            
+          
+        
+
+        
+          
+          
+          
+            
+          
+        
+
+                      
+              Username / Password salah            
+                      
+
+        
+          
+            Login
+          
+        
+      </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/div[@class=&quot;limiter&quot;]/div[@class=&quot;container-login100&quot;]/div[@class=&quot;wrap-login100&quot;]/form[@class=&quot;login100-form validate-form&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//form[@action='https://testingjarkom.000webhostapp.com/services/auth/login.php']</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//form</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Testing UAS PPL2020/Test Suites/Uji Coba New Pasien.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Uji Coba New Pasien</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>f6b64914-1580-41b7-a4a9-92cf3b7bae06</testSuiteGuid>
+   <testCaseLink>
+      <guid>78be886f-db76-4e7e-b12b-f9e18f4bab8c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Pengujian Insert Pasien Variabel</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>aaedc251-cd9f-4fcf-bc1b-59bc7a6f7e25</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Test New Pasien</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>aaedc251-cd9f-4fcf-bc1b-59bc7a6f7e25</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>nama</value>
+         <variableId>c95b21ad-c59c-4811-b286-573cb7303543</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>aaedc251-cd9f-4fcf-bc1b-59bc7a6f7e25</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>nomor rekam</value>
+         <variableId>3cad1b19-7826-493f-8961-780654bb2d0e</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>aaedc251-cd9f-4fcf-bc1b-59bc7a6f7e25</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>nomor hp</value>
+         <variableId>4558d76a-5bea-42f0-a8e7-2c11aed06e1d</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>aaedc251-cd9f-4fcf-bc1b-59bc7a6f7e25</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>alamat</value>
+         <variableId>8e578135-73ad-4aba-8108-01e52cb8c0bb</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
